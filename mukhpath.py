851af3cd58653a9275387957ff8d5729cb3156f0
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2025-01-16 08:21:05 -0600

    Update mukhpath.py

--- mukhpath.py
+++ mukhpath.py
@@ -11,9 +11,9 @@ def compare(a,b) -> [str, float]:
   ratio = SequenceMatcher(None,a,b).ratio()
   return diff_str, ratio
 
-def open_mp_file(file_path):
-  pass
-
+def open_mp_file(file_path : str) -> list:
+  with open(file_path, 'r') as f:
+    return f.readlines()
 
 def test_fx():
   a = 'aj[ yXp&@Pn[ oir ni[bRi'
@@ -26,4 +26,11 @@ def test_fx():
 
 if __name__ == "__main__":
 
-  test_fx()
+  # Mukhpath lines
+  mukhpath = []
+
+  # Open sample.txt
+  with open('sample.txt', 'r') as f:
+    mukhpath = f.readlines()
+
+  print(mukhpath)
