8772dfe0836f1bdab9b6a8ee04d5716d45ecf575
bhavsarp23 <101777921+bhavsarp23@users.noreply.github.com>
2025-01-16 08:19:47 -0600

    Update kivyDemo.py

--- kivyDemo.py
+++ kivyDemo.py
@@ -7,22 +7,25 @@ from kivy.uix.widget import Widget
 from kivy.uix.checkbox import CheckBox
 from kivy.core.clipboard import Clipboard
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 
 import gujhk
 import mukhpath
 import harikrishna
 
-GHANSHYAM_FONT_PATH = '~/.fonts/GHANSHYAM.ttf'
+GHANSHYAM_FONT_PATH = '/home/parthbhavsar/.fonts/GHANSHYAM.ttf'
+GHANSHYAM_FONT_PATH = '/home/parthbhavsar/.fonts/sarjudas.ttf'
+UNICODE_FONT_PATH = '/home/parthbhavsar/.fonts/Noto_Sans_Gujarati/static/NotoSansGujarati-Medium.ttf'
 MARKUP_CLOSER = '[/color]'
 
-class MyApp(App):
+class MukhpathApp(App):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.q_current = 1
-        self.q_total = 1
         self.average_score = 0
-
+        self.mukhpath = mukhpath.open_mp_file('sample.txt')
+        self.q_total = len(self.mukhpath)
 
     def build(self):
 
@@ -39,24 +42,20 @@ class MyApp(App):
         top_level_layout.add_widget(right_panel)
 
         # Mukhpath label
-        mukhpath_label = Label(text='m&Kpiq', font_size='96sp',
+        self.mukhpath_label = Label(text='m&Kpiq', font_size='96sp',
                                font_name=GHANSHYAM_FONT_PATH)
-        middle_panel.add_widget(mukhpath_label)
+        middle_panel.add_widget(self.mukhpath_label)
 
         # Score & options panel
         score_panel = BoxLayout(orientation='horizontal')
         self.score_label = Label(text=f'Question {self.q_current}/{self.q_total}, Average: {self.average_score}')
         virama_box_label = Label(text="Implicitly use viramas")
         self.virama_box = CheckBox()
-        copy_button = Button(text='Copy Output')
-        copy_input = Button(text='Copy Input')
-        copy_button.bind(on_press=self.copy_text)
-        copy_input.bind(on_press=self.copy_input)
+
+        # Add widgets to the score panel
         score_panel.add_widget(self.score_label)
         score_panel.add_widget(virama_box_label)
         score_panel.add_widget(self.virama_box)
-        score_panel.add_widget(copy_button)
-        score_panel.add_widget(copy_input)
         middle_panel.add_widget(score_panel)
 
         # Hint box
@@ -93,12 +92,45 @@ class MyApp(App):
         self.text_input.bind(on_text_validate=self.text_compare)
         middle_panel.add_widget(self.text_input)
 
+        # Control widget
+        control_widget = BoxLayout(orientation='horizontal')
+
+        # Previous button
+        previous_button = Button(text='Previous')
+        previous_button.bind(on_press=self.previous_mp)
+        control_widget.add_widget(previous_button)
+
+        # Copy buttons
+        copy_button = Button(text='Copy Output')
+        copy_input = Button(text='Copy Input')
+        copy_button.bind(on_press=self.copy_text)
+        copy_input.bind(on_press=self.copy_input)
+        control_widget.add_widget(copy_button)
+        control_widget.add_widget(copy_input)
+
+        # Font selection
+        font_select = DropDown()
+        for font in ['GHANSHYAM', 'sarjudas', 'Unicode']:
+            drop_down = Button(text=font, size_hint_y=None, height=44)
+            drop_down.bind(on_press=lambda btn: font_select.select(btn.text))
+            font_select.add_widget(drop_down)
+        font_select.bind(on_select=self.set_font)
+        drop_down = Button(text='Select Font', size_hint_y=None)
+        drop_down.bind(on_release=font_select.open)
+        control_widget.add_widget(drop_down)
+
+        # Next button
+        next_button = Button(text='Next')
+        next_button.bind(on_press=self.next_mp)
+        control_widget.add_widget(next_button)
+
+        middle_panel.add_widget(control_widget)
+
         return top_level_layout
 
     def update_output(self, ascii_text):
         #self.transliterated_output.text = gujhk.ascii_to_guj(ascii_text)
         self.transliterated_output.text = harikrishna.ascii_to_harikrishna(ascii_text)
-
 
     def on_text_input(self, instance, value):
         self.update_output(self.text_input.text)
@@ -110,12 +142,20 @@ class MyApp(App):
     def copy_input(self, instance):
         Clipboard.copy(self.text_input.text)
 
+    def set_font(self, instance, font):
+        if 'Unicode' in font:
+            font = UNICODE_FONT_PATH
+        self.transliterated_output.font_name = font
+        self.hint_box.font_name = font
+        self.difference_box.font_name = font
+        self.mukhpath_label.font_name = font
+
     def update_difference_box(self, diff_str : str):
         # Set all text to green
         self.difference_box.color = '#00FF00'
 
         # Escape [ and ] with a backslash
-        diff_str = diff_str.replace('[', '\[').replace(']', '\]')
+        #diff_str = diff_str.replace('[', '\[').replace(']', '\]')
 
         markup_str = []
         i = 0
@@ -134,20 +174,42 @@ class MyApp(App):
 
         # Add markup closing brackets for - and +
         markup_str = ''.join(markup_str).replace('+','[color=ffff00]').replace('-','[color=ff0000]')
-
+\
         self.difference_box.text = markup_str
 
     def update_score(self, ratio : float):
-        self.score_label.text = '{:.2f}'.format(ratio)
+        self.score_label.text = 'Question {}/{}, Avg. Score: {:.2f}'.format(self.q_current, self.q_total, ratio)
+
+    def get_hint(self) -> str:
+        # Return the first three words of the mukhpath
+        return harikrishna.ascii_to_harikrishna(' '.join(self.mukhpath[self.q_current].split(' ')[0:3]))
+
+    def next_mp(self, instance):
+        self.q_current = (self.q_current + 1) % len(self.mukhpath)
+        # Clear difference box
+        self.difference_box.text = ''
+        # Update the hint
+        self.hint_box.text = self.get_hint()
+        # Update the question
+        self.score_label.text = 'Question {}/{}, Avg. Score {:.2f}'.format(self.q_current+1, self.q_total, self.average_score)
+
+    def previous_mp(self, instance):
+        self.q_current = (self.q_current - 1) % len(self.mukhpath)
+        # Clear difference box
+        self.difference_box.text = ''
+        # Update the hint
+        self.hint_box.text = self.get_hint()
+        # Update the question
+        self.score_label.text = 'Question {}/{}, Avg. Score {:.2f}'.format(self.q_current+1, self.q_total, self.average_score)
 
     def text_compare(self, instance):
         diff_str, ratio = mukhpath.compare(
-                                           self.transliterated_output.text,
-                                           self.hint_box.text
+                                           self.text_input.text,
+                                           self.mukhpath[self.q_current]
         )
-        diff_str = diff_str.replace('   ','128').replace(' ', '').replace('128', ' ')
         self.update_score(ratio)
-        self.update_difference_box(diff_str.replace('', ''))
+        print(diff_str)
+        self.update_difference_box(harikrishna.ascii_to_harikrishna(''.join(diff_str.replace('   ', '128').replace(' ', '').replace('128', ' '))))
 
         # def on_text_change(self, instance):
     #     # Update the output label with the text from the TextInput
@@ -155,4 +217,4 @@ class MyApp(App):
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    MukhpathApp().run()
